a7ffa244ad42b2498ef5f1bb41e78a223c00333b
Angela Sheu <angelasheu.us@gmail.com>
2013-11-17 17:03:12 -0800

    JSON data for classifiers

--- src/scripts/content/content_script.js
+++ src/scripts/content/content_script.js
@@ -4,16 +4,26 @@ var bayes;
 
 bayes = new classifier.Bayesian();
 
-console.log(json.cats);
-
 bayes.backend.catCounts = json.cats;
 bayes.backend.wordCounts = json.words;
+
+//alchemyTest();
 
 function textMining(eventData){
   var target = eventData.target;
   var innerText = target.innerText;
 
   var cat = bayes.classify(innerText);
-  alert(cat + ": " + innerText);
+  //alert(cat + ": " + innerText);
 
+}
+
+function alchemyTest() {
+	var sample_url = 'http://alvaradoschool.net/about/general-education-program/';
+	keywords('url', sample_url, {}, function(error, response) {
+		var keywords = response.keywords;
+		for (var i = 0; i < keywords.length; i++) {
+			console.log(keywords[i]);
+		}
+	})
 }
--- src/scripts/intro/intro.js
+++ src/scripts/intro/intro.js
@@ -1,1 +1,2 @@
-chrome.tabs.executeScript(null, {file: "scripts/content/content_script.js", allFrames : true});
+//chrome.tabs.executeScript(null, {file: "scripts/content/content_script.js", allFrames : true});
+chrome.tabs.executeScript(null, {file: "scripts/content/content_filter.js", allFrames : true});
